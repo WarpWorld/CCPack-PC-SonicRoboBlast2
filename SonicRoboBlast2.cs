c2f4f222b32ca1ce3dc329e7db2a9a898358fc8a
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2024-04-24 16:57:01 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- SonicRoboBlast2.cs
+++ SonicRoboBlast2.cs
@@ -1,8 +1,5 @@
 ﻿using CrowdControl.Common;
 using JetBrains.Annotations;
-using System;
-using System.IO;
-using System.Collections.Generic;
 
 namespace CrowdControl.Games.Packs.SonicRoboBlast2
 {
@@ -19,36 +16,78 @@ namespace CrowdControl.Games.Packs.SonicRoboBlast2
         {
             get
             {
-                List<Effect> effects = new List<Effect>
-                {
-                    new Effect("Spawn Bumper", "bumper") { Price = 10, Description = "Spawns a bumper in the player's way." },
-                    new Effect("Give Rings", "giverings") { Price = 1, Quantity = 99, Description = "Give the player some rings." },
+                List<Effect> effects =
+                [
+                    new Effect("Spawn Bumper", "bumper")
+                        { Price = 10, Description = "Spawns a bumper in the player's way." },
+                    new Effect("Give Rings", "giverings")
+                        { Price = 1, Quantity = 99, Description = "Give the player some rings." },
                     new Effect("Kill", "kill") { Price = 200, Description = "Take a life from the player." },
                     new Effect("Slap", "slap") { Price = 25, Description = "Give the player a good slap" },
-                    new Effect("Give Speedshoes", "sneakers") { Price = 25, Description = "Give the player a pair of speed shoes. Gotta go fast!" },
-                    new Effect("Give Invincibility", "invulnerability") { Price = 25, Description = "Give the player invincibility. How nice of you." },
+                    new Effect("Give Speedshoes", "sneakers")
+                        { Price = 25, Description = "Give the player a pair of speed shoes. Gotta go fast!" },
+                    new Effect("Give Invincibility", "invulnerability")
+                        { Price = 25, Description = "Give the player invincibility. How nice of you." },
 
-                    new Effect("Disable Jump", "nojump") { Duration = 15, Price = 50, Category = "Controls", Description = "Disables the player's jump button." },
-                    new Effect("Disable Spin", "nospin") { Duration = 15, Price = 50, Category = "Controls", Description = "Disables the player's spin button." },
-                    new Effect("Invert Controls", "invertcontrols") { Duration = 15, Price = 50, Category = "Controls", Description = "Inverts the player's controls." },
+                    new Effect("Disable Jump", "nojump")
+                    {
+                        Duration = 15, Price = 50, Category = "Controls",
+                        Description = "Disables the player's jump button."
+                    },
+                    new Effect("Disable Spin", "nospin")
+                    {
+                        Duration = 15, Price = 50, Category = "Controls",
+                        Description = "Disables the player's spin button."
+                    },
+                    new Effect("Invert Controls", "invertcontrols")
+                    {
+                        Duration = 15, Price = 50, Category = "Controls", Description = "Inverts the player's controls."
+                    },
 
-                    new Effect("Spawn Crawla", "crawla") { Price = 10, Category = "Enemies", Description = "Spawns a Crawla around the player." },
-                    new Effect("Spawn Rosy", "rosy") { Price = 10, Category = "Enemies", Description = "Spawns Amy to hug the player." },
-                    new Effect("Spawn Crawla Commander", "commander") { Price = 50, Category = "Enemies", Description = "Spawns a Crawla Commander around the player." },
+                    new Effect("Spawn Crawla", "crawla")
+                        { Price = 10, Category = "Enemies", Description = "Spawns a Crawla around the player." },
+                    new Effect("Spawn Rosy", "rosy")
+                        { Price = 10, Category = "Enemies", Description = "Spawns Amy to hug the player." },
+                    new Effect("Spawn Crawla Commander", "commander")
+                    {
+                        Price = 50, Category = "Enemies", Description = "Spawns a Crawla Commander around the player."
+                    },
 
-                    new Effect("Give Pity Shield", "pityshield") { Price = 10, Category = "Shields", Description = "Grants the player a basic shield." },
-                    new Effect("Give Fire Shield", "fireshield") { Price = 10, Category = "Shields", Description = "Grants the player a fire shield." },
-                    new Effect("Give Bubble Shield", "bubbleshield") { Price = 10, Category = "Shields", Description = "Grants the player a bubble shield. BWAOH" },
-                    new Effect("Give Lightning Shield", "lightningshield") { Price = 10, Category = "Shields", Description = "Grants the player a fire shield." },
+                    new Effect("Give Pity Shield", "pityshield")
+                        { Price = 10, Category = "Shields", Description = "Grants the player a basic shield." },
+                    new Effect("Give Fire Shield", "fireshield")
+                        { Price = 10, Category = "Shields", Description = "Grants the player a fire shield." },
+                    new Effect("Give Bubble Shield", "bubbleshield")
+                    {
+                        Price = 10, Category = "Shields", Description = "Grants the player a bubble shield. BWAOH"
+                    },
+                    new Effect("Give Lightning Shield", "lightningshield")
+                    {
+                        Price = 10, Category = "Shields", Description = "Grants the player a fire shield."
+                    },
 
-                    new Effect("Change to Sonic", "changesonic") { Price = 10, Category = "Characters", Description = "Sets the player character to Sonic." },
-                    new Effect("Change to Tails", "changetails") { Price = 10, Category = "Characters", Description = "Sets the player character to Tails." },
-                    new Effect("Change to Knuckles", "changeknuckles") { Price = 10, Category = "Characters", Description = "Sets the player character to Knuckles." },
-                    new Effect("Change to Amy", "changeamy") { Price = 10, Category = "Characters", Description = "Sets the player character to Amy." },
-                    new Effect("Change to Fang", "changefang") { Price = 10, Category = "Characters", Description = "Sets the player character to Fang." },
-                    new Effect("Change to Metal Sonic", "changemetal") { Price = 10, Category = "Characters", Description = "Sets the player character to Metal Sonic." },
-                    new Effect("Change to Random Character", "changerandom") { Price = 10, Category = "Characters", Description = "Sets the player character to a random character." },
-                };
+                    new Effect("Change to Sonic", "changesonic")
+                        { Price = 10, Category = "Characters", Description = "Sets the player character to Sonic." },
+                    new Effect("Change to Tails", "changetails")
+                        { Price = 10, Category = "Characters", Description = "Sets the player character to Tails." },
+                    new Effect("Change to Knuckles", "changeknuckles")
+                    {
+                        Price = 10, Category = "Characters", Description = "Sets the player character to Knuckles."
+                    },
+                    new Effect("Change to Amy", "changeamy")
+                        { Price = 10, Category = "Characters", Description = "Sets the player character to Amy." },
+                    new Effect("Change to Fang", "changefang")
+                        { Price = 10, Category = "Characters", Description = "Sets the player character to Fang." },
+                    new Effect("Change to Metal Sonic", "changemetal")
+                    {
+                        Price = 10, Category = "Characters", Description = "Sets the player character to Metal Sonic."
+                    },
+                    new Effect("Change to Random Character", "changerandom")
+                    {
+                        Price = 10, Category = "Characters",
+                        Description = "Sets the player character to a random character."
+                    }
+                ];
                 return effects;
             }
         }
